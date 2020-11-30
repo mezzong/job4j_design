b821719ab67beecb03f19fb598a18332e45803d3
Stas <bazhoker@gmail.com>
2020-11-30 14:33:32 +0300

    1. Что такое итератор. [#421319]

--- src/main/java/Trigger.java
+++
@@ -1,9 +0,0 @@
-public class Trigger {
-    public static void main(String[] args) {
-        System.out.println(Trigger.hello());
-    }
-
-    public static String hello() {
-        return "hello";
-    }
-}

348351735a3587b86f68e47dde475a179eb8b69c
Stas <bazhoker@gmail.com>
2020-12-03 20:50:12 +0300

    1. Динамический список на массиве. [#421310]

---
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,68 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArray<E> implements Iterable<E> {
+
+    private Object[] container;
+    private int size = 0;
+    private int modCount = 0;
+
+    public SimpleArray() {
+        this.container = new Object[10];
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        return (E) container[index];
+    }
+
+    public void add(E model) {
+        modCount++;
+        if (size == container.length) {
+            container = grow();
+        }
+        container[size] = model;
+        size++;
+    }
+
+    private Object[] grow() {
+        return Arrays.copyOf(container, container.length * 2);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new SimpleArrayIterator();
+    }
+
+    private class SimpleArrayIterator implements Iterator<E> {
+        private int cursor = 0;
+        private final int expectedModCount = modCount;
+
+        SimpleArrayIterator() {
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public E next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size) {
+                throw new NoSuchElementException();
+            }
+            cursor = i + 1;
+            return (E) container[i];
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+        }
+    }
+
+}

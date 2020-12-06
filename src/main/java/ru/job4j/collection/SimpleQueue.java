2593ed18198c3628ed435952e785f202f35a9d9f
Stas <bazhoker@gmail.com>
2020-12-07 01:19:16 +0300

    5. Очередь на двух стеках [#421312]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (out.getSize() == 0) {
+            for (int i = 0; i <= in.getSize(); i++) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}

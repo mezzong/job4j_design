f2556bc4d0732dc912af468becb4d3e8dd7b2203
Stas <bazhoker@gmail.com>
2020-12-16 18:56:17 +0300

    0.1. FileOutputStream. [#421358]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    String s = (i * j) + " ";
+                    out.write(s.getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

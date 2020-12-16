f165f0eee1484692d9ee7bb3be0bcc27a2de5f41
Stas <bazhoker@gmail.com>
2020-12-16 20:27:28 +0300

    0.3. BufferedReader. [#421359]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try(BufferedReader in = new BufferedReader(new FileReader(file))) {
+            while (in.ready()) {
+                String s = in.readLine();
+                String[] line = s.split(" ");
+                String penult = line[line.length - 2];
+                if (Integer.parseInt(penult) == 404) {
+                    result.add(s);
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}

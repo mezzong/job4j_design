24ad9651cb18066100c44b220ec174348b227936
Stas <bazhoker@gmail.com>
2020-12-17 17:13:07 +0300

    1. Читаем файл конфигурации [#421364]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            Map<String, String> result = read.lines()
+                    .filter(x -> !Objects.equals(x, "") && !Objects.equals(x.substring(0, 2), "##"))
+                    .collect(
+                            Collectors.toMap(
+                                    e -> e.split("=")[0],
+                                    e -> e.split("=")[1]
+                            )
+                    );
+            values.putAll(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}

3d18979d6d12087db6ffd88bc161b6a14f3c7fe6
Stas <bazhoker@gmail.com>
2020-12-21 15:52:37 +0300

    5. Валидация параметров запуска. [#421357]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length < 2) {
             throw new IllegalArgumentException(
                     "Provide arguments: first argument is the initial folder, "
                             + "second argument file extension");

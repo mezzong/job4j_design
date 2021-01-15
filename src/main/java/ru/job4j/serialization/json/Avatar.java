9099dacf6b401ec56e82fc011879c07ac05420a1
Stas <bazhoker@gmail.com>
2021-01-15 14:44:04 +0300

    2. Формат JSON [#421371]

---
+++ src/main/java/ru/job4j/serialization/json/Avatar.java
@@ -0,0 +1,16 @@
+package ru.job4j.serialization.json;
+
+public class Avatar {
+    private String url;
+
+    public Avatar(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" +
+                "url='" + url + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/User.java
@@ -0,0 +1,42 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+public class User {
+    private final boolean sex;
+    private final int age;
+    private final String nickname;
+    private final Avatar avatar;
+    private final int[] favoriteNumbers;
+
+    public User(boolean sex, int age, String nickname, Avatar avatar, int... favoriteNumbers) {
+        this.sex = sex;
+        this.age = age;
+        this.nickname = nickname;
+        this.avatar = avatar;
+        this.favoriteNumbers = favoriteNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "sex=" + sex +
+                ", age=" + age +
+                ", nickname='" + nickname + '\'' +
+                ", avatar=" + avatar +
+                ", favoriteNumbers=" + Arrays.toString(favoriteNumbers) +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        User user = new User(true, 25, "nick", new Avatar("/img/avatar.jpg"),1, 2, 31);
+        Gson gson = new GsonBuilder().create();
+        String jsonUser = gson.toJson(user);
+        System.out.println(jsonUser);
+        User fromJson = gson.fromJson(jsonUser, User.class);
+        System.out.println(fromJson);
+    }
+}
